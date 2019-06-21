47d7c1b5c2c8e8b6dd0d817bd42226dccb7271e0
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-06-21 10:40:17 +0100

    Added detailed comments and tidied up

--- led.py
+++ led.py
@@ -4,20 +4,20 @@ from artiq.experiment import*
 def input_led_state() -> TBool: #This defines a function that returns a boolian variable. The input is read from the command line and returned.
     return input("Enter desired LED state: ") == "1"
 	
-class LED(EnvExperiment):
+class LEDKeyedInput(EnvExperiment):
 	def build(self):
         #This sets the device drivers as attributes and adds the key to the kernel invarients (things stored on the FPGA). You have to do this to use the device 
 		self.setattr_device("core")
 		self.setattr_device("led0")
 		self.setattr_device("led1")
-	@kernel
+	@kernel #This decorator tells the system to run the following code on the core device (FPGA) rather than to run it as normal python code on the host (computer)
 	def run(self):
 		self.core.reset()
 		s = input_led_state() #Call the function defined earlier and store the output as "s"
-		self.core.break_realtime() #I don't fully understand how this works but functionally it lets the system wait for an input before resuming real time operation
+		self.core.break_realtime() #This moves the timeline far enough into the future to ensure that the input has been entered before the system tries to check the condition
 		if s:
-			self.led0.on() #switch on the LEDs
+			self.led0.on() #switch on the LED
 			self.led1.on()
 		else:
-			self.led0.off() #switch off the LEDs
+			self.led0.off() #switch off the LED
 			self.led1.off()
--- ledflash.py
+++ ledflash.py
@@ -1,22 +1,20 @@
 from artiq.experiment import*
 
-class LED(EnvExperiment):
-    def build(self):
+class LEDrealTime(EnvExperiment):
+    def build(self): #Adds the device drivers as attributes and adds the keys to the kernel invarients
         self.setattr_device("core")
         self.setattr_device("led0")
         self.setattr_device("led1")
-    @kernel
+    @kernel #Tells the system to run the following on the core device
     def run(self):
         self.core.reset()
-        self.core.break_realtime()
-
         for i in range(10):    
             for i in range(5):
                 delay(300*ms)
-                with parallel:
+                with parallel: #These events happen simultaneously
                     self.led0.pulse(300*ms)
                     self.led1.pulse(300*ms)
                 
-                for i in range(2):
+                for i in range(2): #These events happen sequentially
                     self.led0.pulse(300*ms)
                     self.led1.pulse(300*ms)
