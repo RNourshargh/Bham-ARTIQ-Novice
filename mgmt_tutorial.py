8388175cc67980e23b9e8dd6ad762cb7748714b2
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-07-09 16:14:48 +0100

    updated as in tutorial

--- mgmt_tutorial.py
+++ mgmt_tutorial.py
@@ -4,7 +4,8 @@ from artiq.experiment import *
 class MgmtTutorial(EnvExperiment):
     """Management tutorial"""
     def build(self):
-        pass  # no devices used
+        self.setattr_argument("count", NumberValue(ndecimals=0, step=1))
 
     def run(self):
-        print("Hello World")
+        for i in range(self.count):
+            print("Hello World", i)
