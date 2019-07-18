54645f819588eeabaf8a947a95facb6c39ce2e50
RNourshargh <44952231+RNourshargh@users.noreply.github.com>
2019-07-18 13:44:41 +0100

    updated comments and removed unneccessary dimension from data array

--- CN_Sampler_MinimumWorkingRequirements.py
+++ CN_Sampler_MinimumWorkingRequirements.py
@@ -3,6 +3,7 @@ import numpy as np
 
 
 class Test(EnvExperiment):
+    """Sampler"""
     def build(self):
         self.setattr_device("core")
         self.setattr_device("sampler0")
@@ -10,27 +11,28 @@ class Test(EnvExperiment):
 
     @rpc(flags={"async"})               
     def cb(self, data):
-        self.queue.append(data)                         #adds whatever is passed into it into a queue
+        self.queue.append(data)                         #adds "data" list to queue 
 
     def run(self):
         
-        self.sample()                                   #runs sampler code
+        self.sample()                                   #runs sample function
         data = np.array(self.queue)                     #generates array in current context and populates it with queued values
         
         for i in range(1024):
-            print(data[0][i][0])                        #prints data from channel 0 in the 10th run
+            print(data[0][i])                           #prints data from channel 0 
 
     @kernel
     def sample(self):
-        channels = 8                                    #runs on 8 channels
-        d = [[0]*channels for i in range(1024)]         #d is a  8x1024 int array
-        acqs = len(d)                                   #number of acquiisitions is 1024
+        channels = 8                                    #runs on all 8 channels
+        d = [[0]*channels for i in range(1024)]         #d is a  8x1024 int array used to store sampler data
+        acqs = len(d)                                   #number of samples is given by the length of "d"
         self.core.reset()                               
-        dev = self.sampler0                             #sets card to be run on
+        dev = self.sampler0                             #sets sampler0 a card to be used
         dev.init()                                      #initialises sampler
+        
         for i in range(acqs):                           #loops over number of samples
             dev.sample_mu(d[i])                         #runs sampler, saves to d
-            delay(6*us)                                 #8us delay
+            delay(6*us)                                 #6us delay minimum
         
         self.cb(d)                                      #move data to queue
         self.core.break_realtime()                  
